8de874df57499470b6ad0940c779c9fa704f23ad
jhuei0831 <jhuei0831@gmail.com>
2020-06-24 11:58:43 +0800

    hero detail / service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent],
+  bootstrap:    [ AppComponent ],
+  providers: [HeroService]
 })
 export class AppModule { }
